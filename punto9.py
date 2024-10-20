e18a3bf03e54fcf00679d32613e1ccd1e4a53769
CamiloAcevedoCuevas <ccuevasa@uninorte.edu.co>
2024-10-20 14:28:54 -0500

    Se agrego el método set_dia en la clase Dia y su documentación, se añadió una lista de temperaturas media, se corrigió documentación y se añadió el método add_temp_med en la clase Dia.

--- punto9.py
+++ punto9.py
@@ -14,19 +14,21 @@
 
 class Dia:
     """
-    Esta clase permite almacenar temperaturas máximas, mínimas e ideales de varios días.
+    Esta clase permite almacenar temperaturas máximas, mínimas, ideales y medias de varios días.
 
     Métodos:
         add_temp_max(temp_max): Añade una temperatura máxima.
         add_temp_min(temp_min): Valida y añade una temperatura mínima.
         add_temp_idl(temp_idl): Añade una temperatura ideal.
+        add_temp_med(temp_med): Añade una temperatura media
         get_dias(): Retorna la cantidad de días ingresados.
-        set_dia(): Establece dia en cero.
+        set_dia(): Establece el dia en cero.
 
     Atributos:
         temps_maxs: Lista de temperaturas máximas.
         temps_mins: Lista de temperaturas mínimas.
         temps_idls: Lista de temperaturas ideales.
+        temps_meds: Lista de temperaturas medias.
         dia: Día actual.
         temp_max: Temperatura máxima actual.
         temp_min: Temperatura mínima actual.
@@ -36,6 +38,7 @@ class Dia:
     temps_maxs = []
     temps_mins = []
     temps_idls = []
+    temps_meds = []
     
     def __init__(self):
         self.dia = 1
@@ -58,7 +61,10 @@ class Dia:
 
     def add_temp_idl(self, temp_idl):
         self.temps_idls.append(temp_idl)
-    
+
+    def add_temp_med(self, temp_med):
+        self.temps_meds.append(temp_med)
+
     def get_dias(self):
         while True:
             aux = input(f"\nIngresar datos día {self.dia + 1} ¿(0 = No, 1 = Sí)?: ")
@@ -77,7 +83,7 @@ class Dia:
     
 class Temperatura:
     """
-    Esta clase permite validar temperaturas y almacenar temperaturas medias.
+    Esta clase permite validar temperaturas y conseguir temperaturas medias.
 
     Métodos:
         get_val_temp(temp, str): Valida y retorna una temperatura.
@@ -113,6 +119,7 @@ def main():
         dia.add_temp_max(dia.temp_max)
         dia.temp_min = temp.validar_temp(input("\nIngrese la temperatura mínima: "), "mínima")
         dia.add_temp_min(dia.temp_min)
+        dia.add_temp_med(temp.get_temp_med(dia.dia - 1))
         dia.temp_idl = temp.validar_temp(input("\nIngrese la temperatura ideal: "), "ideal")
         dia.add_temp_idl(dia.temp_idl)
         dias = dia.get_dias()
@@ -120,8 +127,7 @@ def main():
     # Mostrar temperaturas medias
     print("_________________________________________________\n\nTemperaturas medias:\n")
     for d in range(dias):
-        dia.temp_med = temp.get_temp_med(d)
-        print(f"Día--- {d + 1}: {dia.temp_med}°\n")
+        print(f"Día--- {d + 1}: {dia.temps_meds[d]}°\n")
 
     # Mostrar días con menor temperatura
     print("_________________________________________________\n\nDías con menor temperatura:\n")
@@ -131,14 +137,14 @@ def main():
         print(f"Día--- {d + 1}: {sorted[d]}°\n")
 
     # Buscar día(s) con temperatura máxima
-    temp_max = temp.validar_temp(input("_________________________________________________\n\nIngrese la temperatura máxima a buscar: "), "máxima a buscar")
-    if float(temp_max) in dia.temps_maxs:
-        print(f"_________________________________________________\n\nDía(s) con temperatura máxima = {temp_max}°:\n")
+    dia.temp_max = temp.validar_temp(input("_________________________________________________\n\nIngrese la temperatura máxima a buscar: "), "máxima a buscar")
+    if float(dia.temp_max) in dia.temps_maxs:
+        print(f"_________________________________________________\n\nDía(s) con temperatura máxima = {dia.temp_max}°:\n")
         for d in range(dias):
-            if float(temp_max) == dia.temps_maxs[d]:
+            if float(dia.temp_max) == dia.temps_maxs[d]:
                 print(f"Día--- {d + 1}\n")
     else:
-        print(f"_________________________________________________\n\nNo hay días con temperatura máxima = {temp_max}°.\n")
+        print(f"_________________________________________________\n\nNo hay días con temperatura máxima = {dia.temp_max}°.\n")
 
 if __name__ == "__main__":
     main()
