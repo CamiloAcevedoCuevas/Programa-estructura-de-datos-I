6a1d0b2c4fde338bb47332eb10e1e5b07bb8505c
Camilo Acevedo <145441735+CamiloAcevedoCuevas@users.noreply.github.com>
2024-10-21 05:59:58 -0500

    Update punto9.py

--- punto9.py
+++ punto9.py
@@ -85,7 +85,6 @@ class Dia:
             self.dia += 1
             return self.get_dia_men_temp(temp)
 
-    
 class Temperatura:
     """
     Esta clase permite validar temperaturas y conseguir temperaturas medias.
